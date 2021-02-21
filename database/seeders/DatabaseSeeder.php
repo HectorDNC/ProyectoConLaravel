5668e6d26e2f20b62ce19f053649cbe5b621c24d
HectorDNC <hector.noguera03@gmail.com>
2021-02-20 22:16:50 -0430

    Creación de modelos e iniciando Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Servicio;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // Servicio
+        $servicio = new Servicio();
+        $servicio->nombre = "Diagnostico";
+        $servicio->descripcion = "Diagnostico General";
+        $servicio->precio = 1;
+        $servicio->save();
+
+        $servicio2 = new Servicio();
+        $servicio2->nombre = "Formateo de PC";
+        $servicio2->descripcion = "";
+        $servicio2->precio = 2;
+        $servicio2->save();
+
+        $servicio3 = new Servicio();
+        $servicio3->nombre = "Mantenimiento de Camaras";
+        $servicio3->descripcion = "Limpieza, revisión y reparación de camaras";
+        $servicio3->precio = 5;
+        $servicio3->save();
     }
 }
